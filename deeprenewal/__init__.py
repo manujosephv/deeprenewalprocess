51651c2dc73d969359a79d015600065559dc2e6e
Manu Joseph <manujosephv@gmail.com>
2020-10-15 05:46:11 +0530

    Bump version: 0.4.0 → 0.5.0

--- deeprenewal/__init__.py
+++ deeprenewal/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Manu Joseph"""
 __email__ = 'manujosephv@gmail.com'
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 
 # Relative imports
 from .deeprenewal._estimator import DeepRenewalEstimator
