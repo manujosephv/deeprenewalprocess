4de4bfcffadb5fe7930e7aeb08700264e60f75ee
Manu Joseph <manujosephv@gmail.com>
2020-10-16 05:24:14 +0530

    -- deleted old tags and rolled back versions

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/manujosephv/deeprenewal',
-    version='0.8.1',
+    version='0.1.2',
     zip_safe=False,
 )
