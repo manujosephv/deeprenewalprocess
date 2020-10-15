3a80e190d4bfee22ffca1d2c9634ce75427c0460
Manu Joseph <manujosephv@gmail.com>
2020-10-15 22:18:01 +0530

    -- fixed bug in setup.py

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/manujosephv/deeprenewal',
-    version='version='version='0.8.0''',
+    version='0.8.0',
     zip_safe=False,
 )
