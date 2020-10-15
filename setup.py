38ba394ecf8cbc475b86a2cd197589901a7187d0
Manu Joseph <manujosephv@gmail.com>
2020-10-15 22:08:49 +0530

    Bump version: 0.7.1 → 0.7.1

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/manujosephv/deeprenewal',
-    version='0.7.1',
+    version='version='0.7.1'',
     zip_safe=False,
 )
